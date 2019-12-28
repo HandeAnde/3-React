e1ca817bf641ca5959f1b8c8583c89aed2f67b90
Courtney Anderson <iamcourtcourt@yahoo.com>
2019-12-28 13:05:05 -0600

    first

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,24 @@
+    class Student {
+        constructor(name, email, community){
+            this.name = name;
+            this.email = email;
+            this.community = community;
+        }
+    };
+
+    class Bootstrap {
+        constructor(name, level, students=[]){
+            this.name=name;
+            this.level=level;
+            this.students=students;
+        }
+        registerStudent(studentToRegister){
+            if(this.students.filter(x=>x.email === studentToRegister.email).length>0){
+                console.error(`${studentToRegister.email} is already a registered account`);
+            } else{
+                this.students.push(studentToRegister);
+                console.log(`${studentToRegister.name} has registered with the email: ${studentToRegister.email}`);
+                return this.students;
+            }
+    }
+}
