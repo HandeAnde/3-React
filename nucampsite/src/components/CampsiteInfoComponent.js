d64a58a4add4b62a5d44a73ff74ddbac8564017b
HandeAnde <iamcourtcourt@yahoo.com>
2020-01-22 18:22:19 -0600

    Redux Actions and Reducers

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -36,7 +36,7 @@ function RenderCampsite({ campsite }) {
 	);
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
 	if (comments) {
 		return (
 			<div className="col-5-md m-1">
@@ -55,7 +55,7 @@ function RenderComments({ comments }) {
 					</div>
 				))}
 
-				<CommentForm />
+				<CommentForm campsiteId={campsiteId} addComment={addComment} />
 				<div />
 			</div>
 		);
@@ -72,8 +72,8 @@ class CommentForm extends Component {
 	}
 
 	handleSubmit(values) {
-		console.log('Current state is: ' + JSON.stringify(values));
-		alert('Current state is: ' + JSON.stringify(values));
+		this.toggleModal();
+		this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
 	}
 
 	toggleModal() {
@@ -174,7 +174,11 @@ function CampsiteInfo(props) {
 				</div>
 				<div className="row">
 					<RenderCampsite campsite={props.campsite} />
-					<RenderComments comments={props.comments} />
+					<RenderComments
+						comments={props.comments}
+						addComment={props.addComment}
+						campsiteId={props.campsite.id}
+					/>
 				</div>
 			</div>
 		);
