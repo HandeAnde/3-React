8e545b5c85355a4107bd95e9ff3ab8a40707e8cd
HandeAnde <iamcourtcourt@yahoo.com>
2020-01-24 14:15:03 -0600

    fixed fatch

--- nucampsite/src/redux/ActionCreators.js
+++ nucampsite/src/redux/ActionCreators.js
@@ -29,6 +29,16 @@ export const campsitesLoading = () => ({
 	type: ActionTypes.CAMPSITES_LOADING
 });
 
+export const campsitesFailed = (errMess) => ({
+	type: ActionTypes.CAMPSITES_FAILED,
+	payload: errMess
+});
+
+export const addCampsites = (campsites) => ({
+	type: ActionTypes.ADD_CAMPSITES,
+	payload: campsites
+});
+
 export const fetchComments = () => (dispatch) => {
 	return fetch(baseUrl + 'comments')
 		.then(
